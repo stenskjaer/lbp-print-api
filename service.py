2b03b43cc9407f9de9dcea7f72452f3e1f2e5057
Michael Stenskjær Christensen <michael.stenskjaer@gmail.com>
2017-03-31 13:01:14 +0200

    Move forms into separate file
    This is mostly for keeping it out of the way. I don't know if it should
    go back later.

--- service.py
+++ service.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, send_file, Response
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField
-from wtforms import RadioField, StringField
+from forms import TranscriptionForm
 from werkzeug.utils import secure_filename
 
 import lbp_print
@@ -28,33 +26,6 @@ formatter = logging.Formatter('[%(asctime)s] %(name)s %(levelname)s: %(message)s
 ch.setFormatter(formatter)
 root.addHandler(ch)
 
-
-class TranscriptionForm(FlaskForm):
-    xml_upload_remote = RadioField('Upload or get XML from SCTA?', default='remote',
-                                   description='If you want to use a local XML file, you can upload it. Otherwise you '
-                                               'can provide a SCTA resource id.',
-                                   choices=[('remote', 'Remote'), ('upload', 'Upload')])
-    xslt_upload_default= RadioField('Upload or use default XSLT?', default='default',
-                                    description='Supply a custom xslt transformation stylesheet or use the default '
-                                                'system stylesheets. If the default system stylesheets are chosen, '
-                                                'the correct stylesheet will be determined based on the '
-                                                '<code>schemaRef</code> in the XML file.',
-                                    choices=[('default', 'Default'), ('upload', 'Upload')])
-    pdf_tex = RadioField('Return PDF or TeX', description="Choose whether you want to get a PDF or TeX file.",
-                         default='tex', choices=[('tex', 'TeX'), ('pdf', 'PDF')])
-    scta_id = StringField('SCTA Id', description='The SCTA id is the id of the resource that you want to process. '
-                                                 'Resource ids can be identified on <a '
-                                                 'href="http://scta.info">scta.info</a>. For instance <a '
-                                                 'href="http://scta.info/resource/deAnima">scta.info/resource/deAnima'
-                                                 '</a> lists all resource ids of De anima commentaries, '
-                                                 'while <a '
-                                                 'href="http://scta.info/resource/sententia">scta.info/resource'
-                                                 '/sententia</a> lists all Sentence commentaries, and from there the '
-                                                 'resource ids of each the relevant work items.')
-    xml_upload = FileField('XML Upload',
-                           description='The uploaded file must be valid XML and in comliance with the LombardPress '
-                                       'Schema.')
-    xslt_upload = FileField('XSLT Upload')
 
 
 def upload_file(form_data):
